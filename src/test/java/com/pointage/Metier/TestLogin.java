3f2f8762ee8d3ececf7fc9725beafd7a46b640cf
abdelilahbouyebra <abdelilah.bouyebra@gmail.com>
2017-01-22 16:51:12 +0000

    Update TestLogin.java

--- src/test/java/com/pointage/Metier/TestLogin.java
+++ src/test/java/com/pointage/Metier/TestLogin.java
@@ -14,31 +14,5 @@ public class TestLogin {
 	private static ClassPathXmlApplicationContext context;
 	private static LoginMetier daoLogin;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		context=new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
-		daoLogin= (LoginMetier) context.getBean("metierLogin");
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		context.close();
-	}
-
-	@Test
-	public void testFindUser() {
-		Users us=new Users("test", "test");
-     Users  u=daoLogin.FindUser(us);
-     assertTrue(u.getUserName().equals("test"));
-
-  
-	}
-
-	@Test
-	public void testLogin() {
-		Users us=new Users("test", "123");
-		Users model=daoLogin.login(us);
-		assertTrue(model.getPassword().equals(us.getPassword()));
-	}
-
+	
 }
